4d928029f3bff650d311df7ee863738f8b9015f7
K <kaja47@k47.cz>
2012-12-22 20:02:41 +0100

    initial commit

---
+++ src/Atrox/AsyncMysql.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Atrox;
+
+use React\Promise\Deferred;
+
+class AsyncMysql {
+  private $loop;
+  private $makeConnection;
+
+  // todo: there should be connection pool right in this place, but due to 
+  // impeding apocalypse, I had no time to actually write it, because I was in 
+  // my bunker surrounded by canned food and guns
+
+  /**
+   * @param callable fucnction that makes connection
+   * @param React\EventLoop\LoopInterface
+   */
+  function __construct($makeConnection, $loop) {
+    $this->makeConnection = $makeConnection;
+    $this->loop = $loop;
+  }
+
+  /**
+   * @param string
+   * @return React\Promise\PromiseInterface
+   */
+  function query($query) {
+    $conn = call_user_func($this->makeConnection);
+    $conn->query($query, MYSQLI_ASYNC);
+
+    $defered = new Deferred();
+    $resolver = $defered->resolver();
+    $this->loop->addPeriodicTimer(0.002, function ($signature, $loop) use($conn, $resolver) {
+      $links = $errors = $reject = array($conn);
+      mysqli_poll($links, $errors, $reject, 0); // don't wait, just check
+      if (($read = in_array($conn, $links, true)) || ($err = in_array($conn, $errors, true)) || ($rej = in_array($conn, $reject, true))) {
+        if ($read) {
+          $resolver->resolve($conn->reap_async_query());
+        } elseif ($err) {
+          $resolver->reject($conn->error_list);
+        } else {
+          $resolver->reject('Query was rejected');
+        }
+        $loop->cancelTimer($signature);
+        $conn->close();
+      }
+    });
+    return $defered->promise();
+  }
+}
