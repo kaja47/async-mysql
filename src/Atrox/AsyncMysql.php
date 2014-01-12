ccac7cd66fe3bdd65cd66ba7a7380407e8c17753
K <kaja47@k47.cz>
2014-01-12 12:10:52 +0100

    update to react 0.4.x-dev (because we can) + better error checking (because we have to)

--- src/Atrox/AsyncMysql.php
+++ src/Atrox/AsyncMysql.php
@@ -2,15 +2,12 @@
 
 namespace Atrox;
 
+use React\Promise;
 use React\Promise\Deferred;
 
 class AsyncMysql {
   private $loop;
   private $makeConnection;
-
-  // todo: there should be connection pool right in this place, but due to 
-  // impeding apocalypse, I had no time to actually write it, because I was in 
-  // my bunker surrounded by canned food and guns
 
   /**
    * @param callable fucnction that makes connection
@@ -21,31 +18,48 @@ class AsyncMysql {
     $this->loop = $loop;
   }
 
+
+  private function getConnection() {
+    $conn = call_user_func($this->makeConnection);
+    return ($conn === false) ? Promise\reject(new \Exception(mysqli_connect_error())) : Promise\resolve($conn);
+  }
+
+
   /**
    * @param string
    * @return React\Promise\PromiseInterface
    */
   function query($query) {
-    $conn = call_user_func($this->makeConnection);
-    $conn->query($query, MYSQLI_ASYNC);
-
-    $defered = new Deferred();
-    $resolver = $defered->resolver();
-    $this->loop->addPeriodicTimer(0.002, function ($timer) use($conn, $resolver) {
-      $links = $errors = $reject = array($conn);
-      mysqli_poll($links, $errors, $reject, 0); // don't wait, just check
-      if (($read = in_array($conn, $links, true)) || ($err = in_array($conn, $errors, true)) || ($rej = in_array($conn, $reject, true))) {
-        if ($read) {
-          $resolver->resolve($conn->reap_async_query());
-        } elseif ($err) {
-          $resolver->reject($conn->error_list);
-        } else {
-          $resolver->reject('Query was rejected');
-        }
-        $timer->cancel();
-        $conn->close();
+    return $this->getConnection()->then(function ($conn) use ($query) {
+      $status = $conn->query($query, MYSQLI_ASYNC);
+      if ($status === false) {
+        throw new \Exception($mysqli->error);
       }
+
+      $defered = new Deferred();
+
+      $this->loop->addPeriodicTimer(0.001, function ($timer) use ($conn, $defered) {
+        $links = $errors = $reject = array($conn);
+        mysqli_poll($links, $errors, $reject, 0); // don't wait, just check
+        if (($read = in_array($conn, $links, true)) || ($err = in_array($conn, $errors, true)) || ($rej = in_array($conn, $reject, true))) {
+          if ($read) {
+            $result = $conn->reap_async_query();
+            if ($result === false) {
+              $defered->reject(new \Exception($conn->error));
+            } else {
+              $defered->resolve($result);
+            }
+          } elseif ($err) {
+            $defered->reject($conn->error);
+          } else {
+            $defered->reject(new \Exception('Query was rejected'));
+          }
+          $timer->cancel();
+          $conn->close();
+        }
+      });
+
+      return $defered->promise();
     });
-    return $defered->promise();
   }
 }
